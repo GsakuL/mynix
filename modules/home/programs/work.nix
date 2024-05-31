9d1be1d5316b084c6a649b64bb3628977d28625a
GsakuL <6844160+GsakuL@users.noreply.github.com>
2024-05-31 21:14:31 +0200

    "fix" citrix: force single Monitor

--- modules/home/programs/work.nix
+++ modules/home/programs/work.nix
@@ -10,4 +10,10 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [ teams-for-linux ];
+
+  home.sessionVariables = {
+    # https://support.citrix.com/article/CTX209485/how-to-make-a-session-that-spans-multiple-monitors-with-linux-receiver
+    # wfica -span h
+    WFICA_OPTS = "-span 1";
+  };
 }
