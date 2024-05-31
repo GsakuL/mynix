bdd693989b2a0bfe8995e0fbed6fe4d927057388
GsakuL <6844160+GsakuL@users.noreply.github.com>
2024-05-31 15:14:00 +0200

    add query params in bookmarks

--- modules/home/programs/firefox/bookmarks.nix
+++ modules/home/programs/firefox/bookmarks.nix
@@ -1,5 +1,15 @@
 [
   {
+    name = "Games";
+    bookmarks = [
+      {
+        name = "ProtonDB";
+        url = "https://www.protondb.com/search?q=%s";
+        keyword = "protondb";
+      }
+    ];
+  }
+  {
     name = "NixOS";
     bookmarks = [
       {
@@ -8,15 +18,17 @@
       }
       {
         name = "NixOS Wiki";
-        url = "https://nixos.wiki";
+        url = "https://nixos.wiki/index.php?go=Go&search=%s";
         tags = [
           "wiki"
           "nix"
         ];
+        keyword = "nixwiki";
       }
       {
         name = "MyNixOS";
-        url = "https://mynixos.com";
+        url = "https://mynixos.com/search?q=%s";
+        keyword = "mynix";
       }
     ];
   }
