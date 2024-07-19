3fc1dfc5c745b205b6dbcf2ba1c6d5f7d314265b
GsakuL <6844160+GsakuL@users.noreply.github.com>
2024-07-20 00:11:38 +0200

    add audio bookmarklets

--- modules/home/programs/firefox/bookmarks.nix
+++ modules/home/programs/firefox/bookmarks.nix
@@ -1,4 +1,66 @@
+let
+  /*
+    https://gist.github.com/JamesDBartlett3/9820db1be59b38431fee8cf138127e49
+    Credit: Mike Dombrowski (https://mikedombrowski.com/2019/03/fix-youtube-mono-one-ear-audio/)
+
+    https://gist.github.com/meta-ks/d09d3d44959fdf4561f12f22edc7065c
+  */
+  withAudioContextFunc =
+    action:
+    let
+      # random name to avoid potential collision (?)
+      globalAudioContext = "(globalThis || window).myAudioContext_s14d4sd485sa4d44a4d45a4d54a";
+    in
+    ''
+      (function(){
+        var ctx = ${globalAudioContext};
+        if (!ctx)
+        {
+          var video = document.querySelector('video');
+          var audio = new (window.AudioContext || window.webkitAudioContext)();
+
+          var gain = audio.createGain();
+          var elementSource = audio.createMediaElementSource(video);
+
+          elementSource.connect(gain);
+          gain.connect(audio.destination);
+
+          ${globalAudioContext} = ctx = { audio: audio, gain: gain };
+        }
+        ;${action};
+      })()
+    '';
+  volumeBoost = (
+    mult:
+    let
+      m = builtins.toString mult;
+    in
+    {
+      name = "Volume *${m}";
+      url = "javascript:${withAudioContextFunc "ctx.gain.gain.value *= ${m}"}";
+    }
+  );
+in
 [
+  {
+    name = "Nix Toolbar Bookmarks";
+    toolbar = true;
+    bookmarks = [
+      {
+        name = "🔊 Audio";
+        bookmarks = [
+          {
+            name = "Mono Audio";
+            url = "javascript:${withAudioContextFunc "ctx.audio.destination.channelCount = 1"}";
+          }
+          (volumeBoost 3)
+          (volumeBoost 10)
+        ];
+      }
+    ];
+  }
+
+  # Bookmarks Menu
   {
     name = "Quant";
     url = "https://www.qwant.com/?theme=1&hc=0&hti=0&vt=1&b=1&s=0&ch=none&l=en&locale=de_DE&home=daily&si=1&c=blue&eco_suggest=1&client=opensearch&q=%s";
