af4dedace74699bbd83784b103e322f6ae4a96c1
GsakuL <6844160+GsakuL@users.noreply.github.com>
2024-08-30 18:35:14 +0200

    nix auto gc

--- modules/system/hardware/seifenkiste/default.nix
+++ modules/system/hardware/seifenkiste/default.nix
@@ -41,4 +41,10 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  nix.optimise.automatic = true;
+  nix.gc = {
+    automatic = true;
+    options = "";
+  };
 }
