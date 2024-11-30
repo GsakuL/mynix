3aac19c2a79c747955f17336dbaf1e0dca17d60f
GsakuL <6844160+GsakuL@users.noreply.github.com>
2024-11-30 19:46:26 +0100

    more obs

--- modules/home/programs/obs-studio.nix
+++ modules/home/programs/obs-studio.nix
@@ -9,6 +9,19 @@
       wlrobs
       obs-backgroundremoval
       obs-pipewire-audio-capture
+      obs-vkcapture
+      obs-vaapi
     ];
   };
+
+  home.packages = (
+    with pkgs;
+    [
+      gst_all_1.gstreamer
+      gst_all_1.gst-vaapi
+      gst_all_1.gst-plugins-base
+      gst_all_1.gst-plugins-good
+      gst_all_1.gst-libav
+    ]
+  );
 }
