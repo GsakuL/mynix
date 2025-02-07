c851e296cb81fae6bcd8f0a387b5c4ca9df847de
GsakuL <6844160+GsakuL@users.noreply.github.com>
2025-02-07 20:27:06 +0100

    add ff extension

--- modules/home/programs/firefox/extensions.nix
+++ modules/home/programs/firefox/extensions.nix
@@ -4,4 +4,8 @@
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/chrome-mask/latest.xpi";
     installation_mode = "normal_installed";
   };
+  "magnolia@12.34" = {
+    install_url = "https://gitflic.ru/project/magnolia1234/bpc_uploads/blob/raw?file=bypass_paywalls_clean-latest.xpi";
+    installation_mode = "normal_installed";
+  };
 }
