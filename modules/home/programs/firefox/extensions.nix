8b71de860542fafa94d8489497dd1809b8161267
GsakuL <6844160+GsakuL@users.noreply.github.com>
2025-02-07 20:26:47 +0100

    un-force ff extension in policy

--- modules/home/programs/firefox/extensions.nix
+++ modules/home/programs/firefox/extensions.nix
@@ -2,6 +2,6 @@
   #"*".installation_mode = "blocked"; # blocks all addons except the ones specified below
   "chrome-mask@overengineer.dev" = {
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/chrome-mask/latest.xpi";
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
   };
 }
