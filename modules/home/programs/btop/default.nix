4c42d73b7656a35670717b1bba5ad63fc42e5631
GsakuL <6844160+GsakuL@users.noreply.github.com>
2025-05-16 20:33:59 +0200

    forgot to commit: disable btop hack

--- modules/home/programs/btop/default.nix
+++ modules/home/programs/btop/default.nix
@@ -13,8 +13,8 @@
         patches = (old.patches or [ ]) ++ [
           # git diff --no-index btop_draw.cpp btop_draw.mod.cpp | sed -e 's|a/btop_draw.cpp|a/src/btop_draw.cpp|' | sed -e 's|b/btop_draw.mod.cpp|b/src/btop_draw.cpp|' > short_cmd_draw.diff
           # git diff --no-index btop_shared.cpp btop_shared.mod.cpp | sed -e 's|a/btop_shared.cpp|a/src/btop_shared.cpp|' | sed -e 's|b/btop_shared.mod.cpp|b/src/btop_shared.cpp|' > short_cmd_shared.diff
-          ./short_cmd_draw.diff
-          ./short_cmd_shared.diff
+          # ./short_cmd_draw.diff
+          # ./short_cmd_shared.diff
         ];
       }))
     ]
