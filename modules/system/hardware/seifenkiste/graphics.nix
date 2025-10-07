d51b0bb3fba90eb81f751bcee6ec8b83c98f7331
GsakuL <6844160+GsakuL@users.noreply.github.com>
2025-10-07 18:19:03 +0200

    update

--- modules/system/hardware/seifenkiste/graphics.nix
+++ modules/system/hardware/seifenkiste/graphics.nix
@@ -1,14 +1,10 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   hardware.graphics = {
     enable = true;
     ## radv: an open-source Vulkan driver from freedesktop
     enable32Bit = true;
-
-    ## amdvlk: an open-source Vulkan driver from AMD
-    extraPackages = with pkgs; [ amdvlk ];
-    extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
   };
   services.xserver.videoDrivers = [ "amdgpu" ];
 }
