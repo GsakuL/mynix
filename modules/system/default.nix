c692476a86436fdc9da9860810dd89c09d882c18
GsakuL <6844160+GsakuL@users.noreply.github.com>
2025-11-06 12:01:24 +0100

    remove trim-generations.sh in favor of 'nh clean'

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -4,15 +4,6 @@
   lib,
   ...
 }:
-let
-  trim-gen-sh = pkgs.fetchurl {
-    url = "https://gist.githubusercontent.com/MaxwellDupre/3077cd229490cf93ecab08ef2a79c852/raw/ccb39ba6304ee836738d4ea62999f4451fbc27f7/trim-generations.sh";
-    hash = "sha256-kIWTg8FSpNtDyEFr4/I54+GpGjiV2zWPO6WZQU4gEZ8=";
-  };
-  trim-generations-bin = pkgs.writeShellScriptBin "trim-generations" ''
-    source ${trim-gen-sh} "$@"
-  '';
-in
 {
   imports = [
     ./hardware/seifenkiste
@@ -71,6 +62,5 @@ in
     ])
     ++ [
       (pkgs.python3.withPackages (pypkgs: with pypkgs; [ requests ]))
-      trim-generations-bin
     ];
 }
