e46de23f3ddc1b24f2ff4d0ae429994b76cd0dca
GsakuL <6844160+GsakuL@users.noreply.github.com>
2025-11-17 13:12:32 +0100

    fix pipewire-pulse.conf

--- modules/system/sound/pipewire/default.nix
+++ modules/system/sound/pipewire/default.nix
@@ -3,14 +3,18 @@
   # https://docs.pipewire.org/page_pulse_modules.html
   services.pipewire.configPackages =
     let
-      writeConf =
-        n:
-        pkgs.writeTextDir "share/pipewire/pipewire.conf.d/${n}.conf" (
-          builtins.readFile (./. + "/conf.d/${n}.conf")
+      writeConf2 =
+        dir: n:
+        pkgs.writeTextDir "share/pipewire/${dir}/${n}.conf" (
+          builtins.readFile (./. + "/conf.d/${dir}/${n}.conf")
         );
+      writePipewireConf = n: writeConf2 "pipewire.conf.d" n;
+      writePulseConf = n: writeConf2 "pipewire-pulse.conf.d" n;
     in
-    (builtins.map writeConf [
+    (builtins.map writePipewireConf [
       "80-combine-mic"
+    ])
+    ++ (builtins.map writePulseConf [
       "80-pulse-local-tcp"
     ]);
 }
