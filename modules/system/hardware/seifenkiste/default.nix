d89dbc974c25d1ca11604d577f0f2584df28d8e1
GsakuL <6844160+GsakuL@users.noreply.github.com>
2025-12-01 17:30:33 +0100

    switch nix gc to nh

--- modules/system/hardware/seifenkiste/default.nix
+++ modules/system/hardware/seifenkiste/default.nix
@@ -40,12 +40,6 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  nix.optimise.automatic = true;
-  nix.gc = {
-    automatic = true;
-    options = "";
-  };
-
   services.udev = {
     # packages = with pkgs; [ picoprobe-udev-rules ];
     extraRules = ''
--- modules/system/nix-helpers/default.nix
+++ modules/system/nix-helpers/default.nix
@@ -3,6 +3,10 @@
   programs.nh = {
     enable = true;
     flake = builtins.toString ./../../..;
+    clean = {
+      enable = true;
+      extraArgs = "--keep 10 --keep-since 2d --optimize";
+    };
   };
   environment.systemPackages = with pkgs; [
     nvd # for nh
